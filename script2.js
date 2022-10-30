90ea4cff364154611fd339de9239497a9e941bdc
Abhishek Bansal <82691885+sheku4u@users.noreply.github.com>
2022-10-30 19:34:20 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -3,66 +3,82 @@ let btn = document.querySelector("#addTask")
 let ul = document.querySelector("#taskList")
 
 
-
-btn.addEventListener('click' , ()=>{
+// code for click function by button to press the add button and perform the task 
+btn.addEventListener('click', () => {
     let li = document.createElement("li");
-        li.innerHTML = input.value;
-        li.classList.add(".txt");
+    li.innerHTML = input.value;
+    li.classList.add(".txt");
+    ul.append(li);
 
-        ul.append(li);
-        // for complete the task
-        let btn1 = document.createElement("button");
-        btn1.innerHTML = ("✅");
-        li.append(btn1);
-        btn1.classList.add('btns');
+    // for making the check button 
 
-        btn1.addEventListener('click', (ev)=>{
-            li.classList.toggle("completed");
-        });
+    let btn1 = document.createElement("button");
+    btn1.innerHTML = ("✅");
+    li.append(btn1);
+    btn1.classList.add('btns');
 
-        // delete the task 
-        let btn2=document.createElement("button");
-        li.append(btn2);
-        btn2.innerHTML=("🗑️")
-        btn2.classList.add('btns')
+    // for complete the task make the line through on task
 
-        btn2.addEventListener('click', (ev)=>{
-            li.remove();
-        })
+    btn1.addEventListener('click', (ev) => {
+        li.classList.toggle("completed");
+    });
 
-        input.value = ""
+    // Adding the delete button
+
+    let btn2 = document.createElement("button");
+    li.append(btn2);
+    btn2.innerHTML = ("🗑️")
+    btn2.classList.add('btns')
+
+    // for delete the task from the list
+
+    btn2.addEventListener('click', (ev) => {
+        li.remove();
+    })
+
+    // for clear the input and make this ready for next inserting new task
+
+    input.value = ""
 })
 
-document.body.addEventListener("keydown",(ev)=> {
-    if(ev.key == "Enter"){
+// same thing with the help of ("Enter Button")
+document.body.addEventListener("keydown", (ev) => {
+    if (ev.key == "Enter") {
         let li = document.createElement("li");
         li.innerHTML = input.value;
         li.classList.add(".txt");
 
         ul.appendChild(li);
-        // for complete the task
 
+        // for making the check button 
 
-     let btn1 = document.createElement("button");
+        let btn1 = document.createElement("button");
         btn1.innerHTML = ("✅");
         li.append(btn1);
         btn1.classList.add('btns');
-        btn1.classList.add('btns1');
 
-        btn1.addEventListener('click', (ev)=>{
+        // for complete the task make the line through on task
+
+        btn1.addEventListener('click', (ev) => {
             li.classList.toggle("completed");
         });
 
 
-        // delete the task 
-        let btn2=document.createElement("button");
+        // Adding the delete button
+
+        let btn2 = document.createElement("button");
         li.append(btn2);
-        btn2.innerHTML='🗑️'
+        btn2.innerHTML = '🗑️'
         btn2.classList.add('btns')
 
-        btn2.addEventListener('click', (ev)=>{
+        // for delete the task from the list
+
+        btn2.addEventListener('click', (ev) => {
             li.remove();
         })
+
+        // for clear the input and make this ready for next inserting new task
+
         input.value = ""
 
     }
